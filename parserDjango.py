9484ece2cfcafaad1a92a4fc6631a49782c75793
Bonse <84657555+lxthxrgx@users.noreply.github.com>
2023-03-29 19:20:53 +0300

    Update parserDjango.py

--- parserDjango.py
+++ parserDjango.py
@@ -2,11 +2,100 @@ from openpyxl import *
 import sqlite3 as sq
 import json
 import requests
+import re
+import time 
+
+url_proxy = 'https://advanced.name/freeproxy/64243e3f50649'#free proxy
+
+response = requests.get(url_proxy.encode()) 
+proxy = response.content.decode()
+
+proxy_filter = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}'
+proxy_list = re.findall(proxy_filter, proxy)
+
+for i_http in proxy_list:
+    i_http = ('http://' +  i_http)
+
+for i_https in proxy_list:
+    i_https = ('https://' + i_https)
+
+headers_list = [
+    # Firefox 77 Mac
+     {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "https://www.google.com/",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
+    },
+    # Firefox 77 Windows
+    {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://www.google.com/",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
+    },
+    # Chrome 83 Mac
+    {
+        "Connection": "keep-alive",
+        "DNT": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Dest": "document",
+        "Referer": "https://www.google.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+    },
+    # Chrome 83 Windows 
+    {
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-User": "?1",
+        "Sec-Fetch-Dest": "document",
+        "Referer": "https://www.google.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
+]
 
 wbook = load_workbook(r'excel\excel sources\parcels.xlsx')
 sheet = wbook['parcels']
+
+for row in sheet.iter_rows(min_col=1, max_col=1):
+	for cell in row:
+		url = 'https://kadastr.live/api/parcels/'+cell.value+'/history/?format=json'
+
+
+response = requests.get(url)
+if (response.status_code == 200):
+	data = response.json()
+	data_str = json.dumps(data)
+	data_filtered_str = data_str.replace('""', r'\"\"')
+	try:
+		data_filtered = json.loads(data_filtered_str)
+					
+	except json.decoder.JSONDecodeError:
+		print('Ошибка декодирования JSON-данных:', url) 
+else:
+	a_null = print('Ошибка получения JSON-данных:', response.status_code,url)				
+
+
 database = sq.connect('ltx.db')
 cursor = database.cursor()
+
 cursor.execute('''CREATE TABLE IF NOT EXISTS django(
 	"id"	TEXT,
 	"cadnum"	TEXT,
@@ -25,17 +114,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS django(
 	"valuation_date"	TEXT
 )''')
 
-for row in sheet.iter_rows(min_col=1, max_col=1):
-	for cell in row:
-		url = 'https://kadastr.live/api/parcels/'+cell.value+'/history/?format=json'
-		response = requests.get(url)
-		if (response.status_code == 200):
-				data = response.json()
-				data_str = json.dumps(data)
-				data_filtered_str = data_str.replace('""', r'\"\"')
-				try:
-					data_filtered = json.loads(data_filtered_str)
-					cursor.execute('''INSERT INTO django(
+cursor.execute('''INSERT INTO django(
 					id,
 					cadnum,
 					category,
@@ -53,13 +132,6 @@ for row in sheet.iter_rows(min_col=1, max_col=1):
 					valuation_date)
 					VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', 
 					( data_filtered['id'], data_filtered['cadnum'], data_filtered['category'], data_filtered['area'], data_filtered['unit_area'], data_filtered['koatuu'], data_filtered['use'], data_filtered['purpose'], data_filtered['purpose_code'], data_filtered['ownership'], data_filtered['ownershipcode'], json.dumps(data_filtered['geometry']), data_filtered['address'], data_filtered['valuation_value'], data_filtered['valuation_date']))
-				except json.decoder.JSONDecodeError:
-					print('Ошибка декодирования JSON-данных:', url) 
-		else:
-			a_null = print('Ошибка получения JSON-данных:', response.status_code,url)
-						
-
-print(len(url))
 
 wbook.close()
 database.commit()
